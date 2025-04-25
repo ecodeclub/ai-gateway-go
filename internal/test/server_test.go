8810b0be0b9ce25262ed4b8c3c0bd68737c8d418
yumosx <zhengel2022@163.com>
2025-04-25 23:41:49 +0800

    refactor tests

--- internal/test/server_test.go
+++ internal/test/server_test.go
@@ -16,24 +16,10 @@ import (
 
 type ServerTestSuite struct {
 	suite.Suite
-	ctrl    *gomock.Controller
-	handler *mocks.MockLLMHandler
-	server  *igrpc.Server
 }
 
 func TestServer(t *testing.T) {
 	suite.Run(t, &ServerTestSuite{})
-}
-
-func (s *ServerTestSuite) SetupTest() {
-	s.ctrl = gomock.NewController(s.T())
-	s.handler = mocks.NewMockLLMHandler(s.ctrl)
-	svc := service.NewAIService(s.handler)
-	s.server = igrpc.NewServer(svc)
-}
-
-func (s *ServerTestSuite) TearDownTest() {
-	s.ctrl.Finish()
 }
 
 func (s *ServerTestSuite) TestStream() {
@@ -41,26 +27,33 @@ func (s *ServerTestSuite) TestStream() {
 
 	testcases := []struct {
 		name   string
-		before func()
+		before func(handler *mocks.MockLLMHandler)
 		want   []domain.StreamEvent
 	}{
 		{
 			name: "stream event",
-			before: func() {
+			before: func(handler *mocks.MockLLMHandler) {
 				streamChan := make(chan domain.StreamEvent, 2)
 				streamChan <- domain.StreamEvent{Content: "event1"}
 				streamChan <- domain.StreamEvent{Content: "event2"}
 				close(streamChan)
-				s.handler.EXPECT().StreamHandle(gomock.Any(), gomock.Any()).Return(streamChan, nil)
+				handler.EXPECT().StreamHandle(gomock.Any(), gomock.Any()).Return(streamChan, nil)
 			},
 			want: []domain.StreamEvent{{Content: "event1"}, {Content: "event2"}},
 		},
 	}
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
-			tc.before()
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			handler := mocks.NewMockLLMHandler(ctrl)
+			svc := service.NewAIService(handler)
+			server := igrpc.NewServer(svc)
+
+			tc.before(handler)
 			mockStream := &mocks.MockStreamServer{Ctx: context.Background()}
-			err := s.server.Stream(&ai.LLMRequest{}, mockStream)
+			err := server.Stream(&ai.LLMRequest{}, mockStream)
 			require.NoError(t, err)
 
 			for i, event := range tc.want {
@@ -75,22 +68,27 @@ func (s *ServerTestSuite) TestInvoke() {
 
 	testcases := []struct {
 		name   string
-		before func()
+		before func(handler *mocks.MockLLMHandler)
 		want   domain.LLMResponse
 	}{
 		{
 			name: "stream event",
-			before: func() {
+			before: func(handler *mocks.MockLLMHandler) {
 				resp := domain.LLMResponse{Content: "event1"}
-				s.handler.EXPECT().Handle(gomock.Any(), gomock.Any()).Return(resp, nil)
+				handler.EXPECT().Handle(gomock.Any(), gomock.Any()).Return(resp, nil)
 			},
 			want: domain.LLMResponse{Content: "event1"},
 		},
 	}
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
-			tc.before()
-			invoke, err := s.server.Invoke(context.Background(), &ai.LLMRequest{Id: "1", Text: "hello"})
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			handler := mocks.NewMockLLMHandler(ctrl)
+			svc := service.NewAIService(handler)
+			server := igrpc.NewServer(svc)
+			tc.before(handler)
+			invoke, err := server.Invoke(context.Background(), &ai.LLMRequest{Id: "1", Text: "hello"})
 			require.NoError(t, err)
 			require.Equal(t, tc.want.Content, invoke.Content)
 		})
