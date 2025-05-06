05f62eb767387023a919030e5b63cb3ef57cde8d
yumosx <zhengel2022@163.com>
2025-05-06 20:46:09 +0800

    change the metadata type

--- internal/domain/node.go
+++ internal/domain/node.go
@@ -1,20 +1,29 @@
 package domain
 
+import (
+	"github.com/ecodeclub/ekit"
+)
+
 type Node struct {
 	ID       int64
+	GraphID  int64
 	Type     string
 	Status   string
-	Metadata map[string]any // 用于存储扩展属性
+	Metadata ekit.AnyValue // 用于存储扩展属性
 }
 
 type Edge struct {
+	Id       int64
+	GraphID  int64
 	SourceID int64
 	TargetID int64
 	Weight   float64
-	Metadata map[string]any // 用于存储边的扩展属性
+	Metadata ekit.AnyValue // 用于存储边的扩展属性
 }
 
 type Graph struct {
-	Nodes []Node
-	Edges []Edge
+	Id       int64
+	Nodes    []Node
+	Edges    []Edge
+	Metadata ekit.AnyValue
 }
--- internal/repository/dao/node.go
+++ internal/repository/dao/node.go
@@ -1,19 +1,30 @@
 package dao
 
 type Node struct {
-	ID       int64          `gorm:"column:id;primaryKey;autoIncrement"`
-	Type     string         `gorm:"column:type"`
-	Statue   string         `gorm:"column:statue"`
-	Metadata map[string]any `gorm:"column:metadata;type:json"`
-	Ctime    int64          `gorm:"column:ctime"`
-	Utime    int64          `gorm:"column:utime"`
+	ID       int64  `gorm:"column:id;primaryKey;autoIncrement"`
+	GraphID  int64  `gorm:"column:GraphID"`
+	Type     string `gorm:"column:type"`
+	Status   string `gorm:"column:status"`
+	Metadata string `gorm:"column:metadata"`
+	Ctime    int64  `gorm:"column:ctime"`
+	Utime    int64  `gorm:"column:utime"`
 }
 
 type Edge struct {
-	ID       int64          `gorm:"column:id;primaryKey;autoIncrement"`
-	SourceID int64          `gorm:"column:source_id;index:idx_source_target"`
-	TargetID int64          `gorm:"column:target_id;index:idx_source_target"`
-	Metadata map[string]any `gorm:"column:metadata;type:json"`
-	Ctime    int64          `gorm:"column:ctime"`
-	Utime    int64          `gorm:"column:utime"`
+	ID       int64  `gorm:"column:id;primaryKey;autoIncrement"`
+	GraphID  int64  `gorm:"column:GraphID"`
+	SourceID int64  `gorm:"column:source_id;index:idx_source_target"`
+	TargetID int64  `gorm:"column:target_id;index:idx_source_target"`
+	Metadata string `gorm:"column:metadata;"`
+	Ctime    int64  `gorm:"column:ctime"`
+	Utime    int64  `gorm:"column:utime"`
+}
+
+type Graph struct {
+	ID       int64  `gorm:"column:id;primaryKey;autoIncrement"`
+	Edges    []Edge `gorm:"column:foreignKey:GraphID"`
+	Nodes    []Node `gorm:"column:foreignKey:GraphID"`
+	MetaData string `gorm:"column:metadata"`
+	Ctime    int64  `gorm:"column:ctime"`
+	Utime    int64  `gorm:"column:utime"`
 }
