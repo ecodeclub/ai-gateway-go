ac91cb7959ccd627e6a617d10a96d557fb893b6b
ian <141902143+yumosx@users.noreply.github.com>
2025-05-19 12:14:01 +0800

    添加node, edge, graph 的定义 (#11)
    * 添加domain的定义
    
    * change the metadata type
    
    * change the model and domain
    
    * add save and get api
    
    * add tablename method and initTable
    
    * finish all graph apis
    
    * rm weight
    
    * rename getgraphvo
    
    * update
    
    * fix

---
+++ internal/web/graph.go
@@ -0,0 +1,136 @@
+package web
+
+import (
+	"github.com/ecodeclub/ai-gateway-go/internal/domain"
+	"github.com/ecodeclub/ai-gateway-go/internal/service"
+	"github.com/ecodeclub/ekit"
+	"github.com/ecodeclub/ginx"
+	"github.com/ecodeclub/ginx/session"
+	"github.com/gin-gonic/gin"
+	"github.com/gotomicro/ego/core/elog"
+	"go.uber.org/zap"
+)
+
+type GraphHandler struct {
+	svc *service.NodeService
+}
+
+func NewGraphHandler(nodeSvc *service.NodeService) *GraphHandler {
+	return &GraphHandler{svc: nodeSvc}
+}
+
+func (h *GraphHandler) PrivateRoutes(engine *gin.Engine) {
+	graph := engine.Group("/graph")
+	graph.POST("/save", ginx.BS[SaveGraphReq](h.SaveGraph))
+	graph.POST("/delete", ginx.BS[DeleteReq](h.DeleteGraph))
+	graph.POST("/detail", ginx.BS[GetReq](h.GetGraph))
+
+	node := engine.Group("/node")
+	node.POST("/save", ginx.BS[Node](h.SaveNode))
+	node.POST("/delete", ginx.BS[DeleteReq](h.DeleteNode))
+
+	edge := engine.Group("/edge")
+	edge.POST("/save", ginx.BS[Edge](h.SaveEdge))
+	edge.POST("/delete", ginx.BS[DeleteReq](h.DeleteEdge))
+}
+
+func (h *GraphHandler) PublicRoutes(engine *gin.Engine) {
+
+}
+
+func (h *GraphHandler) GetGraph(ctx *ginx.Context, req GetReq, sess session.Session) (ginx.Result, error) {
+	graph, err := h.svc.GetGraph(ctx, req.ID)
+	if err != nil {
+		elog.Error("获取graph 失败", elog.Int64("ID", req.ID), elog.Any("err", err))
+		return ginx.Result{Code: 500, Msg: "内部错误"}, ginx.ErrNoResponse
+	}
+	return ginx.Result{Msg: "OK", Data: newGetNodeVO(graph)}, err
+}
+
+func (h *GraphHandler) SaveGraph(ctx *ginx.Context, req SaveGraphReq, sess session.Session) (ginx.Result, error) {
+	graph := domain.Graph{
+		ID: req.ID,
+	}
+
+	id, err := h.svc.SaveGraph(ctx, graph)
+	if err != nil {
+		elog.Error("保存 Graph 失败", elog.Int64("ID", req.ID), elog.Any("err", err))
+		return ginx.Result{Code: 500, Msg: "内部错误"}, ginx.ErrNoResponse
+	}
+
+	return ginx.Result{Msg: "OK", Data: id}, nil
+}
+
+func (h *GraphHandler) SaveNode(ctx *ginx.Context, req Node, sess session.Session) (ginx.Result, error) {
+	node := domain.Node{
+		ID:       req.ID,
+		GraphID:  req.GraphID,
+		Type:     req.Type,
+		Status:   req.Status,
+		Metadata: ekit.AnyValue{Val: req.Metadata},
+	}
+
+	id, err := h.svc.SaveNode(ctx, node)
+	if err != nil {
+		elog.Error("保存 Node 失败", elog.Int64("ID", req.ID), elog.Any("err", err))
+		return ginx.Result{Code: 500, Msg: "内部错误"}, ginx.ErrNoResponse
+	}
+	return ginx.Result{Data: id}, nil
+}
+
+func (h *GraphHandler) SaveEdge(ctx *ginx.Context, req Edge, sess session.Session) (ginx.Result, error) {
+	edge := domain.Edge{
+		ID:       req.ID,
+		GraphID:  req.GraphID,
+		TargetID: req.TargetID,
+		SourceID: req.SourceID,
+		Metadata: ekit.AnyValue{Val: req.Metadata},
+	}
+
+	id, err := h.svc.SaveEdge(ctx, edge)
+	if err != nil {
+		elog.Error("保存 Edge 失败", elog.Int64("ID", req.ID), elog.Any("err", err))
+		return ginx.Result{Code: 500, Msg: "内部错误"}, ginx.ErrNoResponse
+	}
+
+	return ginx.Result{
+		Data: id,
+	}, nil
+}
+
+func (h *GraphHandler) DeleteNode(ctx *ginx.Context, req DeleteReq, sess session.Session) (ginx.Result, error) {
+	id := req.ID
+
+	err := h.svc.DeleteNode(ctx, id)
+	if err != nil {
+		elog.Error("删除 Node 失败", elog.Int64("ID", req.ID), elog.Any("err", err))
+		return ginx.Result{Code: 500, Msg: "内部错误"}, ginx.ErrNoResponse
+	}
+	return ginx.Result{
+		Msg: "OK",
+	}, nil
+}
+
+func (h *GraphHandler) DeleteEdge(ctx *ginx.Context, req DeleteReq, sess session.Session) (ginx.Result, error) {
+	id := req.ID
+	err := h.svc.DeleteEdge(ctx, id)
+	if err != nil {
+		elog.Error("删除 Edge 失败", elog.Int64("ID", req.ID), zap.Any("err", err))
+		return ginx.Result{Code: 500, Msg: "内部错误"}, ginx.ErrNoResponse
+	}
+	return ginx.Result{
+		Msg: "OK",
+	}, nil
+}
+
+func (h *GraphHandler) DeleteGraph(ctx *ginx.Context, req DeleteReq, sess session.Session) (ginx.Result, error) {
+	id := req.ID
+	err := h.svc.DeleteGraph(ctx, id)
+	if err != nil {
+		elog.Error("删除 Graph 失败", elog.Int64("ID", req.ID), elog.Any("err", err))
+		return ginx.Result{Code: 500, Msg: "内部错误"}, ginx.ErrNoResponse
+	}
+	return ginx.Result{
+		Msg: "OK",
+	}, nil
+}
--- internal/web/vo.go
+++ internal/web/vo.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"github.com/ecodeclub/ai-gateway-go/internal/domain"
+	"github.com/ecodeclub/ekit/slice"
 )
 
 type PromptVO struct {
@@ -80,6 +81,52 @@ type UpdatePromptReq struct {
 	ID          int64  `json:"id,omitempty"`
 	Name        string `json:"name,omitempty"`
 	Description string `json:"description,omitempty"`
+}
+
+type SaveGraphReq struct {
+	ID    int64  `json:"id"`
+	Steps []Node `json:"steps"`
+	Edges []Edge `json:"edges"`
+}
+
+type GraphVO struct {
+	ID    int64  `json:"id"`
+	Nodes []Node `json:"steps"`
+	Edges []Edge `json:"edges"`
+}
+
+type GetReq struct {
+	ID int64 `json:"id"`
+}
+
+type Node struct {
+	ID       int64  `json:"id,omitempty"`
+	GraphID  int64  `json:"graph_id,omitempty"`
+	Type     string `json:"type,omitempty"`
+	Status   string `json:"status,omitempty"`
+	Metadata string `json:"metadata,omitempty"`
+}
+
+type Edge struct {
+	ID       int64  `json:"id,omitempty"`
+	GraphID  int64  `json:"graph_id,omitempty"`
+	SourceID int64  `json:"source_id,omitempty"`
+	TargetID int64  `json:"target_id,omitempty"`
+	Metadata string `json:"metadata,omitempty"`
+}
+
+func newGetNodeVO(plan domain.Graph) GraphVO {
+	var vo GraphVO
+	vo.ID = plan.ID
+	vo.Nodes = slice.Map[domain.Node, Node](plan.Steps, func(idx int, src domain.Node) Node {
+		m, _ := src.Metadata.AsString()
+		return Node{ID: src.ID, Type: src.Type, Status: src.Status, Metadata: m, GraphID: src.GraphID}
+	})
+	vo.Edges = slice.Map[domain.Edge, Edge](plan.Edges, func(idx int, src domain.Edge) Edge {
+		m, _ := src.Metadata.AsString()
+		return Edge{ID: src.ID, TargetID: src.TargetID, SourceID: src.SourceID, Metadata: m, GraphID: src.GraphID}
+	})
+	return vo
 }
 
 type UpdateVersionReq struct {
