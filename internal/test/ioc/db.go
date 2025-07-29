c561469ed57336a01d6c446a5433dcbfa1724cbe
Deng Ming <ming_flycash@qq.com>
2025-07-29 23:32:16 +0800

    修复测试问题

--- internal/test/ioc/db.go
+++ internal/test/ioc/db.go
@@ -15,10 +15,14 @@
 package ioc
 
 import (
+	"context"
+	"database/sql"
 	"log"
 	"os"
+	"time"
 
 	"github.com/ecodeclub/ai-gateway-go/internal/repository/dao"
+	"github.com/ecodeclub/ekit/retry"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -30,6 +34,7 @@ func InitDB() *gorm.DB {
 		dsn = "root:root@tcp(localhost:13306)/ai_gateway_platform"
 	}
 	log.Print("测试 MySQL：" + dsn)
+	WaitForDBSetup(dsn)
 	db, err := gorm.Open(mysql.Open(dsn))
 	if err != nil {
 		panic(err)
@@ -39,4 +44,32 @@ func InitDB() *gorm.DB {
 		panic(err)
 	}
 	return db
+}
+
+func WaitForDBSetup(dsn string) {
+	sqlDB, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	const maxInterval = 10 * time.Second
+	const maxRetries = 10
+	strategy, err := retry.NewExponentialBackoffRetryStrategy(time.Second, maxInterval, maxRetries)
+	if err != nil {
+		panic(err)
+	}
+
+	const timeout = 5 * time.Second
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		err = sqlDB.PingContext(ctx)
+		cancel()
+		if err == nil {
+			break
+		}
+		next, ok := strategy.Next()
+		if !ok {
+			panic("WaitForDBSetup 重试失败......")
+		}
+		time.Sleep(next)
+	}
 }
