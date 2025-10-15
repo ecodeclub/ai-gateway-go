f98ac9371fade7ff85fc74d07d83102d3f377140
longyue0521 <longyueli0521@gmail.com>
2025-10-15 13:33:59 +0800

    实现面试体验版，支持多轮对话+总体评价

--- internal/service/stream/fcall/kbase/kbase_rag.go
+++ internal/service/stream/fcall/kbase/kbase_rag.go
@@ -59,9 +59,9 @@ func (k *RAG) Call(ctx *domain.StreamContext, req fcall.Request) (fcall.Response
 	response := httpx.NewRequest(ctx.Ctx, http.MethodPost, k.url).
 		Client(k.client).JSONBody(ragReq.Query).Do()
 
-	if response.Err() != nil {
-		return fcall.Response{}, fmt.Errorf("执行知识库查询失败, err: %v", response.Err())
-	}
+	//if response.Err() != nil {
+	//	return fcall.Response{}, fmt.Errorf("执行知识库查询失败, err: %v", response.Err())
+	//}
 
 	if response.StatusCode != http.StatusOK {
 		return fcall.Response{}, fmt.Errorf("执行知识库查询失败, status code: %d", response.StatusCode)
